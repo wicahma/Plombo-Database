695b75d7f45bec971f2158b760a929bf67690492
wicahma <teguh.dc.kusuma@gmail.com>
2023-03-20 23:05:37 +0700

    bugL cors origin v.12312312

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-MongoConnection();
-
-const userRouter = require("./src/routes/userRoute");
-const artikelRouter = require("./src/routes/artikelRoute");
-const wisataRouter = require("./src/routes/wisataRoute");
-const komentarRouter = require("./src/routes/komentarRoute");
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
@@ -21,6 +14,15 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"],
   })
 );
+
+MongoConnection();
+
+
+const userRouter = require("./src/routes/userRoute");
+const artikelRouter = require("./src/routes/artikelRoute");
+const wisataRouter = require("./src/routes/wisataRoute");
+const komentarRouter = require("./src/routes/komentarRoute");
+
 
 // methods: ["GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"],
 // "https://plombo-web-application-diama.vercel.app",
