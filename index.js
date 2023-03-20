059128952e3f1fbc6dc9bc879ef46baca474591f
wicahma <teguh.dc.kusuma@gmail.com>
2023-03-20 23:13:34 +0700

    bug: cors origin again and again

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://plombo-web-application.vercel.app/",
+    origin: "https://plombo-web-application.vercel.app",
     methods: "*",
   })
 );
