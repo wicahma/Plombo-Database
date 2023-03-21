b3df14a74ceb3dff20c49e8a24d56352ff038a2c
wicahma <teguh.dc.kusuma@gmail.com>
2023-03-21 21:08:08 +0700

    k

--- index.js
+++ index.js
@@ -9,16 +9,35 @@ const mainRoute = process.env.MAIN_ROUTES;
 MongoConnection();
 
 app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: [
-      "https://plombo-web-application.vercel.app",
-      "https://plombo-web-application-diama.vercel.app",
-      "https://plombo-web-application-git-master-diama.vercel.app",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "https://plombo-web-application.vercel.app",
+//       "https://plombo-web-application-diama.vercel.app",
+//       "https://plombo-web-application-git-master-diama.vercel.app",
+//     ],
+//     methods: ["GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"],
+//   })
+// );
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", [
+    "https://plombo-web-application.vercel.app",
+    "https://plombo-web-application-diama.vercel.app",
+    "https://plombo-web-application-git-master-diama.vercel.app",
+  ]);
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
+});
 
 app.use(express.urlencoded({ extended: true }));
 
