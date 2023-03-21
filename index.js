41428fef76592e1d0a4f3650fea694c998b5603a
wicahma <teguh.dc.kusuma@gmail.com>
2023-03-21 18:22:48 +0700

    *im tired :(

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 4000;
+const mainRoute = process.env.MAIN_ROUTES;
+MongoConnection();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,7 +17,7 @@ app.use(
   })
 );
 
-MongoConnection();
+app.use(express.urlencoded({ extended: false }));
 
 const userRouter = require("./src/routes/userRoute");
 const artikelRouter = require("./src/routes/artikelRoute");
@@ -26,7 +28,6 @@ const komentarRouter = require("./src/routes/komentarRoute");
 // methods: ["GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"],
 // "https://plombo-web-application-git-master-diama.vercel.app",
 // "https://plombo-web-application.vercel.app",
-const mainRoute = process.env.MAIN_ROUTES;
 
 app.use(mainRoute, userRouter);
 app.use(mainRoute, artikelRouter);
